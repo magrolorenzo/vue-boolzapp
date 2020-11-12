fc25fdf921cad93ba072f928dc79f7e9d4d836bd
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-12 18:24:10 +0100

    Stampa messaggi della chat

--- assets/main.js
+++ assets/main.js
@@ -7,44 +7,44 @@ var app = new Vue({
         chats:[
             {
                 name: "Boolean#18",
-                // avatar: '_1',
                 messages:[
-                    {
-                        text: "Ciao",
-                        sent: true
-                    },
-                    {
-                        text: "Posso Chiamarti?",
-                        sent: true
-                    },
-                    {
-                        text: "No",
-                        sent: false
-                    },
-                    {
-                        text: "Ora non posso parlare..",
-                        sent: false
-                    },
-                    {
-                        text: "Ok ci sentiamo domani!",
-                        sent: true
-                    }
+                    {text: "Ciao", sent: true},
+                    {text: "Posso Chiamarti?", sent: true},
+                    {text: "No", sent: false},
+                    {text: "Ora non posso parlare..", sent: false},
+                    {text: "Ok ci sentiamo domani!", sent: true}
                 ]
             },
 
             {
                 name: "Omar Peretti",
-                avatar: '_2'
+                messages:[
+                    {text: "Ciao Lollo", sent: false},
+                    {text: "Hey dimmi", sent: true},
+                    {text: "Domani sera ci sei?", sent: false},
+                    {text: "No mi spiace", sent: true},
+                    {text: "Peccato", sent: false}
+                ]
             },
 
             {
                 name: "Riccardo Tamassia",
-                avatar: '_3'
+                messages:[
+                    {text: "Passo a prenderti io?", sent: false},
+                    {text: "Si grazie mille", sent: true},
+                    {text: "Ok 10 minuti e sono li", sent: false},
+                    {text: "Ok io porto la birra", sent: true},
+                    {text: "Scendi!", sent: false}
+                ]
             },
 
             {
                 name: "Mattia Cesta",
-                avatar: '_4'
+                messages:[
+                    {text: "Porti fuori il cane dopo?", sent: false},
+                    {text: "Si tra 20 minuti", sent: true},
+                    {text: "Ok porto pure il mio!", sent: false}
+                ]
             }
         ]
     },
