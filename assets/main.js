2435e056f9e9c6e487ceeb69d9556500fbce6f69
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-13 15:41:41 +0100

    Milestone 3 ok

--- assets/main.js
+++ assets/main.js
@@ -4,14 +4,14 @@ var app = new Vue({
 
     data:{
 
+        // Indice di posizione della chat corrente
         current_chat: 0,
 
+        // Stringa di supporto per salvataggio nuvo messaggio
         new_text_message: "",
+        // Var di supporto per misurare lunghezza array messaggi della chat
+        last_message_pos:0,
 
-        new_message: {
-            text: "",
-            sent: true
-        },
 
         chats:[
             {
@@ -62,20 +62,55 @@ var app = new Vue({
     methods:{
 
         select_chat(selected_contact, contact_index){
-            app.current_chat = contact_index;
+            this.current_chat = contact_index;
         },
 
-        send_message(new_text, chat_index){
+        send_message(){
 
-            this.new_message.text = new_text;
+            // Oggetto messaggio di supporto da pushare nel array corrispondente
+            let new_message_obj = {
+                text: "",
+                sent: true
+            };
 
-            console.log(this.new_message);
-            console.log(this.new_message.text);
-            console.log(chat_index);
+            // assegno all oggetto di supporto la nuva stringa
+            new_message_obj.text = this.new_text_message;
 
-            this.chats[chat_index].messages.splice(-1, 0, this.new_message);
-            this.new_message.text = "";
+            // Mi salvo l'ultima posizione dell'array dei messaggi della chat corente
+            let last_message_pos = (this.chats[this.current_chat].messages.length);
+            console.log("Ultima posizioe = " + last_message_pos);
+
+            // Inserisco il nuovo oggetto messaggio nell'array
+            this.chats[this.current_chat].messages.splice(last_message_pos, 0, new_message_obj);
+
+            // Svuoto il nuvo messaggio
+            this.new_text_message = "";
+
+            // Chiamo la funzione di risposta automatica
+            this.answer_me();
+        },
+
+        // Metodo richiamato per la risposta automatica dopo i 3 secondi
+        answer_me(){
+            // Salvo l'indeice della chat in cui ho scritto la risposta
+            // Se si scrive e poi si cambia chat la risposta verrà visualizzata sulla chat corretta
+            let involved_chat = this.current_chat;
+
+            setTimeout(() => {
+
+                // Oggetto messaggio di risposta automatica
+                let new_message_obj = {
+                    text: "Ok!",
+                    sent: false
+                };
+
+
+                let last_message_pos = (this.chats[involved_chat].messages.length);
+                // Inserisco il nuovo oggetto messaggio nell'array
+                this.chats[involved_chat].messages.splice(last_message_pos, 0, new_message_obj);
+            },1000);
         }
+
     }
 
 });
